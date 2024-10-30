d2e53ddc02603b610715b4d4cb3c376de85ac9fb
Aashif M <164191930+aashifm1@users.noreply.github.com>
2024-10-30 18:00:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 from streamlit_lottie import st_lottie
 from PIL import Image
 
-st.set_page_config(page_title="Sample Webpage using Python", page_icon="images/favicon.ico",layout="wide")
+st.set_page_config(page_title="Portfolio Web App using Python", page_icon="images/favicon.ico",layout="wide")
 
 def load_lottieurl(url):
     r=requests.get(url)
